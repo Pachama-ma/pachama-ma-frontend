0fb1085389dbb7243e9feec35ca4b9ad4d64b7e6
geleeroyale <krassvs@gmail.com>
2023-11-18 18:56:17 +0300

    load typefaces

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -7,10 +7,19 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import ConnectButton from './ConnectButton';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Pachamama',
+};
 
 export default function Header() {
   return (
     <Box sx={{ flexGrow: 1 }}>
+      <link
+        href='https://fonts.googleapis.com/css2?family=Playfair+Display&family=Public+Sans:wght@100&display=swap'
+        rel='stylesheet'
+      />
       <AppBar position='static'>
         <Toolbar>
           <IconButton

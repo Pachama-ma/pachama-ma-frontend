1066dad776cffa2479603f40336c48bfe4e6162c
alireza-sharifpour <ascode94@gmail.com>
2023-11-19 01:35:48 +0300

    created attestations style for user

--- app/user/[ref]/page.tsx
+++ app/user/[ref]/page.tsx
@@ -1,5 +1,7 @@
+import { Container, Stack, Typography } from '@mui/material';
 import { DEFAULT_REVALIDATE_TIME } from '../../config';
 import { getAllRecipientAttestations } from '../../eas/getAllRecipientAttestations';
+import { decodedDataJsonFormatter } from '@/app/utils/decodedDataJsonFormatter';
 
 export default async function UserPage({
   params,
@@ -8,9 +10,8 @@ export default async function UserPage({
 }) {
   // Ref is user address or ens name
   const { ref } = params;
-
+  console.log('Params', params);
   let address: string | null = ref;
-
   if (!address) {
     return (
       <div className='absolute top-0 flex flex-col justify-center h-screen'>
@@ -22,11 +23,24 @@ export default async function UserPage({
   }
 
   const attestations = await getAllRecipientAttestations(address);
-
+  const extracted = decodedDataJsonFormatter(attestations[0].decodedDataJson);
+  console.log('Extracted', extracted);
   return (
     <>
-      <div>User</div>
-      <pre>{JSON.stringify(attestations, null, 2)}</pre>
+      <Stack>
+        <Stack direction={'row'} gap={1}>
+          <Typography variant='body1'>Name: </Typography>
+          <Typography variant='body1'>{extracted.name}</Typography>
+        </Stack>
+        <Stack direction={'row'} gap={1}>
+          <Typography variant='body1'>Level: </Typography>
+          <Typography variant='body1'>{extracted.level}</Typography>
+        </Stack>
+        <Stack direction={'row'} gap={1}>
+          <Typography variant='body1'>Stewardship: </Typography>
+          <Typography variant='body1'>{extracted.stewardship}</Typography>
+        </Stack>
+      </Stack>
     </>
   );
 }

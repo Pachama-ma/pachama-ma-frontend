b90044a5962cac32889873bcaf5161304be2c62e
alireza-sharifpour <ascode94@gmail.com>
2023-11-19 02:45:23 +0300

    Added functionality for adding member

--- app/components/community/AddMemberModal.tsx
+++ app/components/community/AddMemberModal.tsx
@@ -8,16 +8,60 @@ import {
   TextField,
   Typography,
 } from '@mui/material';
-import Grid from '@mui/material/Unstable_Grid2';
 
-import React from 'react';
+import { EAS, SchemaEncoder } from '@ethereum-attestation-service/eas-sdk';
+
+import React, { useState } from 'react';
+import { useSigner } from '@/app/utils/easWagmiUtils';
 
 interface IAddMemberModalProps {
   open: boolean;
   handleClose: () => void;
 }
 
+const easContractAddress = '0xC2679fBD37d54388Ce493F1DB75320D236e1815e';
+const schemaUID =
+  '0x21602b6e2dcffa7acfe555f9e4cc7ecbb77a60fc60fb57b42a68a9a59b75857c';
+const eas = new EAS(easContractAddress);
+
 const AddMemberModal = ({ open, handleClose }: IAddMemberModalProps) => {
+  const [name, setName] = useState('');
+  const [level, setLevel] = useState('0'); // Default level is '0'
+  const [stewardship, setStewardship] = useState('');
+
+  const schemaEncoder = new SchemaEncoder(
+    'string communityUid,string name,uint8 level,string stewardship'
+  );
+  const signer = useSigner();
+  eas.connect(signer as any);
+
+  const encodedData = schemaEncoder.encodeData([
+    { name: 'communityUid', value: '', type: 'string' },
+    { name: 'name', value: name, type: 'string' },
+    { name: 'level', value: level, type: 'uint8' },
+    { name: 'stewardship', value: stewardship, type: 'string' },
+  ]);
+
+  async function attestMember() {
+    if (!signer) return;
+    try {
+      const tx = await eas.attest({
+        schema: schemaUID,
+        data: {
+          recipient: '0x0000000000000000000000000000000000000000',
+          revocable: true, // Be aware that if your schema is not revocable, this MUST be false
+          data: encodedData,
+        },
+      });
+      console.log('tx', tx);
+      const newAttestationUID = await tx.wait();
+      console.log('New attestation UID:', newAttestationUID);
+      console.log('signer', signer);
+    } catch (error) {
+      console.log('Errrrrro,', error);
+    }
+  }
+
   return (
     <Modal open={open} onClose={handleClose}>
       <Box sx={style}>
@@ -25,14 +69,37 @@ const AddMemberModal = ({ open, handleClose }: IAddMemberModalProps) => {
           Add member
         </Typography>
         <Stack spacing={2}>
-          <TextField id='outlined-basic' label='Name' variant='standard' />
-          <TextField id='outlined-basic' label='Level' variant='standard' />
+          <TextField
+            id='outlined-basic'
+            label='Name'
+            variant='standard'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <TextField
+            id='outlined-basic'
+            label='Level'
+            variant='standard'
+            value={level}
+            onChange={(e) => {
+              if (e.target.value.toString().length < 1) return;
+              setLevel(e.target.value.toString());
+            }}
+          />
           <TextField
             id='outlined-basic'
             label='Stewardship'
             variant='standard'
+            onChange={(e) => setStewardship(e.target.value)}
           />
-          <Button onClick={handleClose} variant='contained'>
+          <Button
+            onClick={() => {
+              attestMember().then(() => {
+                handleClose();
+              });
+            }}
+            variant='contained'
+          >
             Add
           </Button>
         </Stack>

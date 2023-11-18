4b1f5a081a83386993ac88330a6325401cc8aa96
alireza-sharifpour <ascode94@gmail.com>
2023-11-18 17:46:30 +0300

    added Land

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from '@mui/material';
 import Header from './components/Header';
+import Land from './components/Land';
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <Land />
     </main>
   );
 }
